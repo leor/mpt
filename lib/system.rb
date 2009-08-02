723a4d809101abfded481feb0c8966d1d5ac00b3
Anatoly Lapshin <technologymasta@gmail.com>
2009-08-02 12:58:01 +0500

    cache_method helper

--- lib/system.rb
+++ lib/system.rb
@@ -8,3 +8,20 @@ class Object
     mod.send :undef_method, :mpt_instance_with_args_handler
   end
 end
+
+class Class
+  def cache_method(method_name, cache_name = nil)
+    cache_attr_name = cache_name
+    cache_attr_name = "#{method_name.to_s}_cache" if cache_name.blank?
+
+    class_eval(<<-EOS, __FILE__, __LINE__)
+      def #{method_name.to_s}_with_cache_support(*args)
+        @#{cache_attr_name} ||= {}
+        @#{cache_attr_name}[args] ||= #{method_name.to_s}_without_cache_support( *args )
+      end
+
+      alias_method_chain :#{method_name.to_s}, :cache_support
+    EOS
+
+  end
+end

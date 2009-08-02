e065e6d4116c37d1a078b62c9b91396de0bdae8c
Anatoly Lapshin <technologymasta@gmail.com>
2009-08-02 12:04:44 +0500

    Refactoring FilterChain array subclass

--- lib/event.rb
+++ lib/event.rb
@@ -1,4 +1,32 @@
 
+class Array
+  def mpt_filterchain_reorganize
+    index = 0
+    while index < self.size
+      filter = self[index]
+      filter_opts = filter[:options]
+      unless filter_opts[:after].nil?
+        for i in index..self.size-1
+          fo = self[i][:options]
+          unless fo[:as].nil?
+            if fo[:as] == filter_opts[:after]
+              new_chain = []
+              new_chain += self[0..index-1] if index > 0
+              new_chain += [self[i]]
+              new_chain += self[index..i-1]
+              new_chain += self[i+1..self.size-1]
+              self.replace new_chain 
+              index = 0
+              break
+            end # if :as matched with :after
+          end	# unless :as is nil				
+        end # for next items
+      end # unless :after is nil
+      index += 1
+    end # while index < list size
+  end # def reorganize
+end # filter chain class
+
 module MPT
   class EventContainer
     attr_accessor :event_object
@@ -9,42 +37,14 @@ module MPT
     
   end
 
-  class FilterChain < Array
-    def reorganize
-      index = 0
-      while index < self.size
-        filter = self[index]
-        filter_opts = filter[:options]
-        unless filter_opts[:after].nil?
-          for i in index..self.size-1
-            fo = self[i][:options]
-            unless fo[:as].nil?
-              if fo[:as] == filter_opts[:after]
-                new_chain = []
-                new_chain += self[0..index-1] if index > 0
-                new_chain += [self[i]]
-                new_chain += self[index..i-1]
-                new_chain += self[i+1..self.size-1]
-                self.replace new_chain 
-                index = 0
-                break
-              end # if :as matched with :after
-            end	# unless :as is nil				
-          end # for next items
-        end # unless :after is nil
-        index += 1
-      end # while index < list size
-    end # def reorganize
-  end # filter chain class
-
   class Event
     @@mpt_subscribers = {}
     
     class << self
       def subscribe(event_name, options = {}, &block)
-        channel = @@mpt_subscribers[event_name] ||= MPT::FilterChain.new
+        channel = @@mpt_subscribers[event_name] ||= []
         channel << { :options => options, :proc => block }
-        channel.reorganize
+        channel.mpt_filterchain_reorganize
       end
 
       def trigger_with_object(event_name, object, *args)
@@ -70,7 +70,7 @@ module MPT
       
       def clear_owner_subscribers(owner)
         @@mpt_subscribers.each_pair do |event_name, subscribers|
-          @@mpt_subscribers[event_name] = MPT::FilterChain.new + subscribers.select { |s| owner != s[:options][:owner]  }
+          @@mpt_subscribers[event_name] = subscribers.select { |s| owner != s[:options][:owner]  }
         end
       end
     end # end of static section
--- mpt.gemspec
+++ mpt.gemspec
@@ -2,11 +2,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{mpt}
-  s.version = "0.1.3.7"
+  s.version = "0.1.3.8"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 1.2") if s.respond_to? :required_rubygems_version=
   s.authors = ["Anatoly Lapshin"]
-  s.date = %q{2009-07-31}
+  s.date = %q{2009-08-02}
   s.description = %q{Monkey Patching Toolkit}
   s.email = %q{}
   s.extra_rdoc_files = ["CHANGELOG", "lib/event.rb", "lib/mpt.rb", "lib/system.rb", "lib/wrap.rb", "LICENSE", "README"]
@@ -16,12 +16,12 @@ Gem::Specification.new do |s|
   s.rdoc_options = ["--line-numbers", "--inline-source", "--title", "Mpt", "--main", "README"]
   s.require_paths = ["lib"]
   s.rubyforge_project = %q{mpt}
-  s.rubygems_version = %q{1.3.2}
+  s.rubygems_version = %q{1.3.1}
   s.summary = %q{Monkey Patching Toolkit}
 
   if s.respond_to? :specification_version then
     current_version = Gem::Specification::CURRENT_SPECIFICATION_VERSION
-    s.specification_version = 3
+    s.specification_version = 2
 
     if Gem::Version.new(Gem::RubyGemsVersion) >= Gem::Version.new('1.2.0') then
       s.add_runtime_dependency(%q<activesupport>, [">= 0"])
